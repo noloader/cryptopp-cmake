184f30b56bcbca24582ae374a50b0b28a517c4f4
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2022-08-22 13:28:23 +0400

    fix: use correct lib in linker options

--- cmake/ConfigFiles.cmake
+++ cmake/ConfigFiles.cmake
@@ -9,8 +9,8 @@ include(CMakePackageConfigHelpers)
 # ------------------------------------------------------------------------------
 # Generate module config files for cmake and pkgconfig
 # ------------------------------------------------------------------------------
-
 function(_module_cmake_config_files)
+  message(STATUS "[cryptopp] Generating cmake package config files")
   # generate the config file that includes the exports
   configure_package_config_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/config.cmake.in
@@ -26,20 +26,22 @@ function(_module_cmake_config_files)
 endfunction()
 
 function(_module_pkgconfig_files)
+  message(STATUS "[cryptopp] Generating pkgconfig files")
   set(MODULE_PKGCONFIG_FILE cryptopp.pc)
-  if(BUILD_SHARED)
-    set(target cryptopp-shared)
-  elseif(BUILD_STATIC)
-    set(target cryptopp-static)
-  else()
-    return()
+
+  set(MODULE_LINK_LIBS "-lcryptopp")
+  if(CMAKE_BUILD_TYPE EQUAL "Debug")
+    if(BUILD_SHARED)
+      get_target_property(target_debug_postfix cryptopp-shared DEBUG_POSTFIX)
+    elseif(BUILD_STATIC)
+      get_target_property(target_debug_postfix cryptopp-static DEBUG_POSTFIX)
+    endif()
+    if(${target_debug_postfix} MATCHES "-NOTFOUND$")
+      set(target_debug_postfix "")
+    endif()
+    set(MODULE_LINK_LIBS "-lcryptopp${target_debug_postfix}")
   endif()
 
-  get_target_property(target_debug_postfix ${target} DEBUG_POSTFIX)
-  if(${target_debug_postfix} MATCHES "-NOTFOUND$")
-    set(target_debug_postfix "")
-  endif()
-  set(MODULE_LINK_LIBS "-lcryptopp${target_debug_postfix}")
   configure_file(config.pc.in
                  ${CMAKE_CURRENT_BINARY_DIR}/${MODULE_PKGCONFIG_FILE} @ONLY)
 endfunction()
